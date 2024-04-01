6860c852285f52c7b3a6a08d6c2ca2a608384c3d
adimasmudi <adimasmudi@gmail.com>
2024-04-01 21:56:35 +0700

    first commit

---
+++ app/(root)/(home)/recordings/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Recordings = () => {
+  return (
+    <section className="flex size-full flex-col gap-10 text-white">
+      <h1 className="text3xl font-bold">Recordings</h1>
+    </section>
+  );
+};
+
+export default Recordings;
